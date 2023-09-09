5c51e5aa6acd79f0b4cd38131730ffd57bb8fa7f
Ton Almeida <oi@tonalmeida.dev>
2023-09-09 17:06:49 -0300

    section-01

---
+++ section-01/project-02/scripts.js
@@ -0,0 +1,33 @@
+members = [
+  {
+    fname: 'John',
+    lname: 'Lennon',
+    instrument: 'Violão'
+  },
+  {
+    fname: 'George',
+    lname: 'Harrison',
+    instrument: 'Guitarra'
+  }
+]
+
+const handlingForms = {
+  data() {
+    return {
+      members: window.members,
+      newMember: {}
+    }
+  },
+  methods: {
+    addMember: function () {
+      if (this.newMember.fname && this.newMember.lname && this.newMember.instrument) {
+        this.members.push(this.newMember);
+        this.newMember = {};
+      } else {
+        alert('Todos os campos devem ser preenchidos')
+      }
+    }
+  }
+};
+
+Vue.createApp(handlingForms).mount('#app');
